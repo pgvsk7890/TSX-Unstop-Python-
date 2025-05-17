0aa30a06b69ffd0fe029fd4b6479a76379dc8dd7
pgvsk7890 <pgvsaikumar7890@gmail.com>
2025-05-17 19:51:13 +0530

    Add files via upload

--- Task1.py
+++ Task1.py
@@ -1,6 +1,13 @@
-
-
+#Write a simple Python script that prints your name and a greeting.
 name = "Sai Kumar"
 greeting = f"Hello! My name is {name}. Nice to meet you."
-
 print(greeting)
+
+#Write a script with comments explaining different parts of the code. 
+import datetime # Import the 'datetime' module to work with dates and times
+name = "Alice"  # Replace "Alice" with your name if desired
+current_year = datetime.datetime.now().year# Get the current year using the datetime module
+print(f"Hello, {name}! The current year is {current_year}.")# Print a personalized greeting
+
+
+
